173bdafaad096901751cb87ff51dd8566387c12c
Micah Williamson <micahwllmsn@gmail.com>
2014-12-30 09:51:35 -0600

    created service entity

---
+++ importProcesses/importEntities.js
@@ -0,0 +1,13 @@
+module.exports = function(source, dest) {
+	var entities = source.EntityManager.getEntities();
+
+	for(var i = 0; i < entities.length; i++) {
+		var entity = entities[i];
+		dest.log(('+ ' + entity.getName()).blue);
+		if(dest.EntityManager.entityExists(entity)) {
+			dest.warn('Entity Exists: ' + entity.getName());
+		} else {
+			dest.EntityManager.registerEntity(Entity);
+		}
+	}
+};
