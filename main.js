7c5a23be2b1a7742b7cfb237efc8ce73ed1599cc
Micah Williamson <micahwllmsn@gmail.com>
2014-12-30 12:25:00 -0600

    added importEntityGroups import process

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ var snooze = require('snooze');
 snooze.module('snooze-baselib')
 	.registerEntityGroupsFromPath('entities/*.js')
 	.registerEntitiesFromPath('services/*.js')
-	.registerImportProcessesFromPath('importProcesses/*.js');
+	.registerImportProcessesFromPath('importProcesses/importEntityGroups.js')
+	.registerImportProcessesFromPath('importProcesses/importEntities.js');
