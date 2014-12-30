6dc8830dc4cb555b99035228a35302c25a75fb7d
Micah Williamson <micahwllmsn@gmail.com>
2014-12-30 10:58:03 -0600

    import process function incorrect

--- importProcesses/importEntities.js
+++ importProcesses/importEntities.js
@@ -1,13 +1,15 @@
-module.exports = function(source, dest) {
-	var entities = source.EntityManager.getEntities();
+module.exports = function(processes) {
+	return function(source, dest) {
+		var entities = source.EntityManager.getEntities();
 
-	for(var i = 0; i < entities.length; i++) {
-		var entity = entities[i];
-		dest.log(('+ ' + entity.getName()).blue);
-		if(dest.EntityManager.entityExists(entity)) {
-			dest.warn('Entity Exists: ' + entity.getName());
-		} else {
-			dest.EntityManager.registerEntity(Entity);
+		for(var i = 0; i < entities.length; i++) {
+			var entity = entities[i];
+			dest.log(('+ ' + entity.getName()).blue);
+			if(dest.EntityManager.entityExists(entity)) {
+				dest.warn('Entity Exists: ' + entity.getName());
+			} else {
+				dest.EntityManager.registerEntity(Entity);
+			}
 		}
-	}
+	};
 };
