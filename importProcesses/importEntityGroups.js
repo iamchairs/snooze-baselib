b4de56f3b8ae5e52af4e7049213cdab8efc194dd
Micah Williamson <micahwllmsn@gmail.com>
2014-12-30 13:13:37 -0600

    EntityGroups have types, not names

--- importProcesses/importEntityGroups.js
+++ importProcesses/importEntityGroups.js
@@ -6,7 +6,7 @@ module.exports = function(processes) {
 			var entity = entities[i];
 			dest.log(('+ ' + entity.getName()).blue);
 			if(dest.EntityManager.entityGroupExists(entity)) {
-				dest.warn('Entity Group Exists: ' + entity.getName());
+				dest.warn('Entity Group Exists: ' + entity.getType());
 			} else {
 				dest.EntityManager.registerEntityGroup(Entity);
 			}
