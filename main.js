46ba27f4988e9f3639a1e7b406d50be1360f0a0c
Micah Williamson <micahwllmsn@gmail.com>
2014-12-30 10:43:18 -0600

    register paths were invalid

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var snooze = require('snooze');
 
 snooze.module('snooze-baselib')
-	.registerEntityGroupsFromPath('./lib/entities/*.js')
-	.registerEntitiesFromPath('./lib/services/*.js')
-	.registerImportProcessesFromPath('./lib/importProcesses/*.js');
+	.registerEntityGroupsFromPath('entities/*.js')
+	.registerEntitiesFromPath('services/*.js')
+	.registerImportProcessesFromPath('importProcesses/*.js');
