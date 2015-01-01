c395469c131acf505d6a3228e667de907c4b42c7
Micah Williamson <micahwllmsn@gmail.com>
2015-01-01 16:48:07 -0600

    added merge mode config preprocessor

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ var snooze = require('snooze');
 snooze.module('snooze-baselib')
 	.registerEntityGroupsFromPath('entities/*.js')
 	.registerEntitiesFromPath('services/*.js')
-	.registerImportProcessesFromPath('importProcesses/importEntityGroups.js')
-	.registerImportProcessesFromPath('importProcesses/importEntities.js');
+	.registerImportProcessesFromPath('importProcesses/*.js')
+	.registerConfigPreprocessorsFromPath('configPreprocessors/*.js');
