3f0172f1e5d8ffd3762cfd7e6e748f88f21ef174
Micah Williamson <micahwllmsn@gmail.com>
2015-01-04 10:29:22 -0600

    service has a getConfig method

--- entityGroups/service.js
+++ entityGroups/service.js
@@ -9,6 +9,7 @@ Service.compile = function(entity) {
 		entity.instance.$compile();
 	}
 };
+
 Service.registerDependencies = function(entity) {
 	if(typeof entity.constructor === 'function') {
 		entity.dependencies = snooze.Util.getParams(entity.constructor);
@@ -16,9 +17,18 @@ Service.registerDependencies = function(entity) {
 		throw Error('Services expect function constructors. ' + (typeof entity.constructor) + ' given');
 	}
 };
+
 Service.getInject = function(entity) {
 	if(entity.instance.$get) {
 		return entity.instance.$get;
+	}
+
+	return entity.instance;
+};
+
+Service.getConfig = function(entity) {
+	if(entity.instance.$config) {
+		return entity.instance.$config;
 	}
 
 	return entity.instance;
